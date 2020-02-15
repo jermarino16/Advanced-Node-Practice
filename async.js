da2777d4984b5a5407388c8de52e82d785eb91ef
Jeremy Marino <jermarino16@gmail.com>
2020-02-15 16:35:18 +0000

    OS benchmark code

---
+++ async.js
@@ -0,0 +1,20 @@
+//practice benchmarking OS requests in node
+
+const https = require("https");
+
+const start = Date.now();
+
+function doRequest(){
+	https
+		.request("https://www.google.com", res => {
+		res.on("data", () => {});
+		res.on("end", () => {
+			console.log(Date.now() - start);
+		});
+	});
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
