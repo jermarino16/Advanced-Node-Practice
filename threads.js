210e128b3123bbded9ea3cb3eaef0d76317ac1fe
Jeremy Marino <jermarino16@gmail.com>
2020-02-15 16:26:18 +0000

    add more tasks, and change threaad pool size

--- threads.js
+++ threads.js
@@ -1,3 +1,5 @@
+process.env.UV_THREADPOOL_SIZE = 1;//experiment run times with threadpool sizes
+
 const crypto = require("crypto");
 
 //benchmark test
@@ -9,6 +11,18 @@ crypto.pbkdf2("a", "b", 100000, 512, "sha512", () =>{
 //this does not wait for the previous call to finish
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () =>{
 	console.log("2: ", Date.now() - start);
+});
+//this does not wait for the previous call to finish
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () =>{
+	console.log("3: ", Date.now() - start);
+});
+//this does not wait for the previous call to finish
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () =>{
+	console.log("4: ", Date.now() - start);
+});
+//this does not wait for the previous call to finish
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () =>{
+	console.log("5: ", Date.now() - start);
 });
 
 //this proves node is not single threaded
