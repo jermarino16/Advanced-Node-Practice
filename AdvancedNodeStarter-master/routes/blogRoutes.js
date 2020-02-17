e377ecda7ee345d30ff3812fc124293fc76a7df5
Jeremy Marino <jermarino16@gmail.com>
2020-02-17 07:05:01 -0800

    refactoring redis caching

--- AdvancedNodeStarter-master/routes/blogRoutes.js
+++ AdvancedNodeStarter-master/routes/blogRoutes.js
@@ -14,28 +14,9 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    //generalize cachein login thats why we write in here
-    const redis = require("redis");
-    const redisUrl = "redis://127.0.0.1:6379";
-    const clent = redis.createClient(redisUrl);
-    const util = require("util"); //promisify function will make things return promise
-    client.get = util.promisify(client.get);
-
-    //do we have any cached date in redis related to do query?
-    const cachedBlogs = await client.get(req.user.id); //dont have to use a call  back because it returns a promise
-
-    //if yes then respond to the request right away and return
-    if (cachedBlogs){
-      console.log("serving from cache");
-      return res.send(JSON.parse(cachedBlogs));
-    }
-    //if no we need to respond to request and update our cache to store data
-    console.log("serving from mongoDB");
-    const blogs = await Blog.find({ _user: req.user.id });
+    const blogs = await Blog.find({_user: req.user.id});
 
     res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs);
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
---
+++ AdvancedNodeStarter-master/services/cache.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util"); 
+
+const redisUrl = "redis://127.0.0.1:6379";
+const clent = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);//promisify function will make things return promise
+
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function () {
+
+	//create the key with values from query and name
+	const key = JSON.stringify(//json stringify to work with redis
+		Object.assign({}, this.getQuery(), {
+			collection: this.mongooseCollection.name
+		})
+	);
+
+	//see if we have a value for key in redis
+	const cacheValue = await client.get(key);
+	//if we do return it
+	if (cacheValue){
+		console.log(cacheValue);
+	}
+
+	//otherwise issue query and store results in redis then return
+	const result = await exec.apply(this, arguments);
+
+	console.log(result);
+}
