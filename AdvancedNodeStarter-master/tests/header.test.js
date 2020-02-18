174703f83b6abd9fa2ad135c1a3d7fe2644bee0e
Jeremy Marino <jermarino16@gmail.com>
2020-02-17 21:42:16 -0800

    small tests of automating browser

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -1,5 +1,17 @@
+const puppeteer = require ("puppeteer");
+
+
 test("Adds two numbers", () => {
 	const sum = 1 + 2;
 
 	expect(sum).toEqual(3);
+});
+
+test("We can launch a browser", async () => {
+	jest.setTimeout(30000);
+	const browser = await puppeteer.launch({
+		headless: false
+	});
+	const page = await browser.newPage();
+	await page.goto("localhost:3000");
 });
