8faf6e37e3265f2ff5c63f876ae396f7b19325a1
Jeremy Marino <jermarino16@gmail.com>
2020-02-18 07:27:05 -0800

    trying to add before each to make testing DRY

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -1,20 +1,30 @@
 const puppeteer = require ("puppeteer");
 
+// test("Adds two numbers", () => {
+// 	const sum = 1 + 2;
 
-test("Adds two numbers", () => {
-	const sum = 1 + 2;
+// 	expect(sum).toEqual(3);
+// });
 
-	expect(sum).toEqual(3);
-});
+let browser, page; // define globals
+
+// beforeEach(async () =>{
+// 	browser = await puppeteer.launch({
+// 		headless: false
+// 	});
+// 	page = await browser.newPage();
+// 	await page.goto("localhost:3000");
+// });
 
 test("We can launch a browser", async () => {
 	jest.setTimeout(30000);
-	const browser = await puppeteer.launch({
+
+	browser = await puppeteer.launch({
 		headless: false
 	});
-	const page = await browser.newPage();
+	page = await browser.newPage();
 	await page.goto("localhost:3000");
-
+	
 	const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 
 	expect(text).toEqual("Blogster");
