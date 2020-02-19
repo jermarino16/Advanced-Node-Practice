9dc8c2a25f70ea387d51aeb9df4809945582430d
Jeremy Marino <jermarino16@gmail.com>
2020-02-19 11:24:41 -0800

    figured out how to fake cookie sessions to run tests of a user who is logged in

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -18,26 +18,50 @@ beforeEach(async () =>{
 });
 
 afterEach(async () => {
-	await browser.close();
+	// await browser.close();
 });
 
-// test("Check the header says blogster", async () => {
-// 	const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+test("Check the header says blogster", async () => {
+	const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 
-// 	expect(text).toEqual("Blogster");
-// });
+	expect(text).toEqual("Blogster");
+});
 
-// test("Check the header has Login with Google", async () => {
-// 	const text = await page.$eval(".right a", el => el.innerHTML);
+test("Check the header has Login with Google", async () => {
+	const text = await page.$eval(".right a", el => el.innerHTML);
 
-// 	expect(text).toEqual("Login With Google");
-// });
+	expect(text).toEqual("Login With Google");
+});
 test("Clicking Login starts oauth flow", async () => {
 	await page.click(".right a");
 	const url = await page.url();
 
 	// expect(url).toContain("accounts.google.com"); //either one works
 	expect(url).toMatch("/accounts\.google\.com/");
+});
+
+test.only("When signed in, shows logout button", async () => {
+	//take an existing user ID and generate a session object with it
+	const id = "5e483e683c01114068c094b6"; //id of user in mongodb atlas
+
+	const Buffer = require("safe-buffer").Buffer;
+	const sessionObject = {
+		passport: {
+			user: id
+		}
+	};
+	const sessionString = Buffer.from(
+		JSON.stringify(sessionObject)).toString("base64");
+
+	const Keygrip = require("keygrip");
+	const keys = require("../config/keys");
+	const keygrip = new Keygrip([keys.cookieKey]);
+	const sig = keygrip.sign("session=" + sessionString);
+	// console.log(sessionString, sig);
+
+	await page.setCookie({ name: "session", value: sessionString });
+	await page.setCookie({ name: "session.sig", value: sig});
+	await page.goto("localhost:3000");
 });
 
 
