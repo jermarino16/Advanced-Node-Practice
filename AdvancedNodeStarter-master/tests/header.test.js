eb879970ad5eca01843e9c52d4e13e40374acf89
Jeremy Marino <jermarino16@gmail.com>
2020-02-18 20:50:01 -0800

    testing that header has Login With Google

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -21,19 +21,16 @@ afterEach(async () => {
 	await browser.close();
 });
 
-test("Check the header has the correct content", async () => {
-	// jest.setTimeout(30000);
-
-	// browser = await puppeteer.launch({
-	// 	headless: false
-	// });
-
-	// page = await browser.newPage();
-	// await page.goto("localhost:3000");
-
+test("Check the header says blogster", async () => {
 	const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 
 	expect(text).toEqual("Blogster");
+});
+
+test("Check the header has login with google", async () => {
+	const text = await page.$eval(".right a", el => el.innerHTML);
+
+	expect(text).toEqual("Login With Google");
 });
 
 
