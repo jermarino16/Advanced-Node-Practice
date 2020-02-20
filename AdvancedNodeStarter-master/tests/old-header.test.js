3ae41556f6b7dd612e6507a4da6da184b1d3d585
Jeremy Marino <jermarino16@gmail.com>
2020-02-19 16:29:03 -0800

    working header tests before refactor

---
+++ AdvancedNodeStarter-master/tests/old-header.test.js
@@ -0,0 +1,59 @@
+const puppeteer 		= require ("puppeteer");
+const sessionFactory 	= require("./factories/sessionFactory");
+const userFactory 		= require("./factories/userFactory");
+const Page = require("./helpers/page");
+
+let browser, page; // define globals
+
+beforeEach(async () =>{
+	jest.setTimeout(30000);
+	browser = await puppeteer.launch({
+		headless: false
+	});
+	page = await browser.newPage();
+	await page.goto("localhost:3000");
+});
+
+afterEach(async () => {
+	await browser.close();
+});
+
+test.only("Check the header says blogster", async () => {
+	const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+
+	expect(text).toEqual("Blogster");
+});
+
+test("Check the header has Login with Google", async () => {
+	await page.waitFor(".right a");
+	const text = await page.$eval(".right a", el => el.innerHTML);
+
+	expect(text).toEqual("Login With Google");
+});
+
+test("Clicking Login starts oauth flow", async () => {
+	await page.waitFor(".right a");
+	await page.click(".right a");
+	const url = await page.url();
+
+	// expect(url).toContain("accounts.google.com"); //either one works
+	expect(url).toMatch("/accounts\.google\.com/");
+});
+
+test("When signed in, shows logout button", async () => {
+	//generate a user and simulate login
+	const user = await userFactory();
+	const { session, sig } = sessionFactory(user);
+
+	await page.setCookie({ name: "session", value: session });
+	await page.setCookie({ name: "session.sig", value: sig});
+	await page.goto("localhost:3000");
+	await page.waitFor("a[href='/auth/logout']");
+
+	const text = await page.$eval("a[href='/auth/logout']", el => el.innerHTML);
+
+	expect(text).toEqual("Logout");
+
+});
+
+
