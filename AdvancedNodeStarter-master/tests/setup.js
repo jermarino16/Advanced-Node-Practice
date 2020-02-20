289acbc87ef14d13874256e0611a3b03e866ea10
Jeremy Marino <jermarino16@gmail.com>
2020-02-20 07:31:15 -0800

    add timeout

--- AdvancedNodeStarter-master/tests/setup.js
+++ AdvancedNodeStarter-master/tests/setup.js
@@ -1,8 +1,9 @@
-require("../models/User");
+jest.setTimeout(30000);
 
-const mongoose 		= require("mongoose");
-const keys 			= require("../config/keys");
+require('../models/User');
 
-mongoose.Promise = global.Promise; //telling mongo to make use of the node js promise object
-mongoose.connect(keys.mongoURI, { useMongoClient: true }); //deprecation warning
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
