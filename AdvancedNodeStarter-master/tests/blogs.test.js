156ecfbd1ae7a612147cab34fcc22a740252d798
Jeremy Marino <jermarino16@gmail.com>
2020-02-20 07:56:28 -0800

    valid input tests show review page and redirect user

--- AdvancedNodeStarter-master/tests/blogs.test.js
+++ AdvancedNodeStarter-master/tests/blogs.test.js
@@ -22,4 +22,39 @@ describe('When logged in', async () => {
 
     expect(label).toEqual('Blog Title');
   });
+
+  describe("and using invalid inputs", async () => {
+  	beforeEach(async () => {
+  		await page.click("form button");
+  	});
+  	test("the form shows an error message", async () =>{
+  		const titleError = await page.getContentsOf('.title .red-text');
+  		const contentError = await page.getContentsOf('.content .red-text');
+
+  		expect(titleError).toEqual('You must provide a value');
+  		expect(contentError).toEqual('You must provide a value');
+  	});
+  });
+  describe("and using valid inputs", async () => {
+  	beforeEach(async () => {
+  		await page.click("form button");
+  	});
+  	test("submitting takes user to a review screen", async () =>{
+  		await page.type(".title input", "My title");
+  		await page.type(".content input", "My Content");
+  		await page.click("form button");
+
+  		const text = await page.getContentsOf('h5');
+
+  		expect(text).toEqual('Please confirm your entries');
+  	});
+  	test("submitting then savings takes user to blog screen", async () =>{
+  		const titleError = await page.getContentsOf('.title .red-text');
+  		const contentError = await page.getContentsOf('.content .red-text');
+
+  		expect(titleError).toEqual('You must provide a value');
+  		expect(contentError).toEqual('You must provide a value');
+  	});
+  });
+
 });
