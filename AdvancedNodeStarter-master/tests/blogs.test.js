de1e5c15f53d950fb9cb622b97c4d46218d0314b
Jeremy Marino <jermarino16@gmail.com>
2020-02-20 07:40:52 -0800

    working testing of checking if blog create form is working

--- AdvancedNodeStarter-master/tests/blogs.test.js
+++ AdvancedNodeStarter-master/tests/blogs.test.js
@@ -2,20 +2,24 @@ const Page = require('./helpers/page');
 
 let page;
 
-
 beforeEach(async () => {
-	page = await Page.build();
-	await page.goto('localhost:3000')
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
 });
+
 afterEach(async () => {
-	await page.close();
+  await page.close();
 });
 
-test('can see blog create form', async () => {
-	await page.login();
-	await page.click('a.btn-floating');
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
 
-	const label = await page.getContentsOf('form label');
+  test('can see blog create form', async () => {
+    const label = await page.getContentsOf('form label');
 
-	expect(label).toEqual('Blog Title');
+    expect(label).toEqual('Blog Title');
+  });
 });
