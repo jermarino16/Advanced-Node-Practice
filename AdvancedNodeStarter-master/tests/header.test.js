5adec42ca4b4e6a0caa82faffc32e3722ce420a8
Jeremy Marino <jermarino16@gmail.com>
2020-02-19 16:25:48 -0800

    working tests if ran one by one

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -1,12 +1,7 @@
 const puppeteer 		= require ("puppeteer");
 const sessionFactory 	= require("./factories/sessionFactory");
 const userFactory 		= require("./factories/userFactory");
-
-// test("Adds two numbers", () => {
-// 	const sum = 1 + 2;
-
-// 	expect(sum).toEqual(3);
-// });
+const Page = require("./helpers/page");
 
 let browser, page; // define globals
 
@@ -23,7 +18,7 @@ afterEach(async () => {
 	await browser.close();
 });
 
-test("Check the header says blogster", async () => {
+test.only("Check the header says blogster", async () => {
 	const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 
 	expect(text).toEqual("Blogster");
