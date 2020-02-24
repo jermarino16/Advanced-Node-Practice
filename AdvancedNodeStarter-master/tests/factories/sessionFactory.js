4e17ae60a064aa37c681a6eaa7e5779cd2e01287
Jeremy Marino <jermarino16@gmail.com>
2020-02-23 18:14:35 -0800

    copying stephen griders code at his github to find error

--- AdvancedNodeStarter-master/tests/factories/sessionFactory.js
+++ AdvancedNodeStarter-master/tests/factories/sessionFactory.js
@@ -3,7 +3,6 @@ const Keygrip = require('keygrip');
 const keys = require('../../config/keys');
 const keygrip = new Keygrip([keys.cookieKey]);
 
-
 module.exports = user => {
   const sessionObject = {
     passport: {
@@ -13,6 +12,5 @@ module.exports = user => {
   const session = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
   const sig = keygrip.sign('session=' + session);
 
-  // return { session: session, sig: sig };
   return { session, sig };
 };
